eee001de3d33a89eb94572356edae22e61e854fe
Emily <sonemily+github@gmail.com>
2023-02-06 19:07:53 -0500

    Fixed Fishing Behavior to patrol.

--- Assets/Scripts/FishBehavior.cs
+++ Assets/Scripts/FishBehavior.cs
@@ -2,37 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class FishBehavior : MonoBehaviour
 {   
     [SerializeField] float moveSpeed = 1f;
-
+    Vector3 fishStartPoint;
     Rigidbody2D myRigidBody;
+    [SerializeField] bool isFacingRight = true;
+    public bool bobberBite = true;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        fishStartPoint = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(IsFacingRight())
+        var distanceFromOriginal = transform.position.x - fishStartPoint.x;
+
+        if((isFacingRight && distanceFromOriginal >= 2) || (!isFacingRight&& distanceFromOriginal <= -2))
         {
-            myRigidBody.velocity = new Vector2(moveSpeed, 0f);
-        } else 
-        {
-            myRigidBody.velocity = new Vector2(-moveSpeed, 0f);
+            TurnFish();
         }
+        var newSpeed = isFacingRight ? moveSpeed : -moveSpeed;
+        myRigidBody.velocity = new Vector2(newSpeed, 0f);
     }
 
-    private bool IsFacingRight() 
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        return transform.localScale.x > Mathf.Epsilon;
+        // var newSign = -(Mathf.Sign(myRigidBody.velocity.x));
+        // transform.localScale = new Vector(newSign * transform.localScale.x, transform.localScale.y, transform.localScale.z);
+
+        // TurnFish();
+
+        Player player = collision.GetComponent<Player>();
+
+        if(player)
+        {
+            player.numFish++;
+            Destroy(this.gameObject);
+        }
+        // run whenever a fish is successfully reeled in, rather than colliding
+
+
+
+        // grab the Bobber component and if there's a collision with the fish, transform the fish
     }
 
-    private void OnTriggerExit2D(Collider2D collision) 
+    private void TurnFish()
     {
-        transform.localScale = new Vector2(-(Mathf.Sign(myRigidBody.velocity.x)), transform.localScale.y);
+        isFacingRight = !isFacingRight;
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
     }
 }
+
+
+
+// do these each step by step
+
+// make the fish parent a child of the bobber using Transform.parent when hooked
+// new state for when the Fish is fighting with the bobber, stop having it move left and right
+// add force to Bobber as the Fish fights it, have it stay still for a bit, then move down and right (opposite the direction of the line)
+// make the force SLIGHTLY stronger than the line
